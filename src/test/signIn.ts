4d28ec717a1e23b67c919a7f962884ed6fdaa338
lazymisha <misha_trunov@hotmail.com>
2022-01-11 21:52:20 +0200

    added routing

---
+++ src/test/signIn.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+export default () => {
+    // Build a JWT payload.  { id, email }
+    const payload = {
+        id: new mongoose.Types.ObjectId().toHexString(),
+        email: 'test@test.com',
+    };
+
+    // Create the JWT!
+    const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+    // Build session Object. { jwt: MY_JWT }
+    const session = { jwt: token };
+
+    // Turn that session into JSON
+    const sessionJSON = JSON.stringify(session);
+
+    // Take JSON and encode it as base64
+    const base64 = Buffer.from(sessionJSON).toString('base64');
+
+    // return a string that's the cookie with the encoded data
+    return [`express:sess=${base64}`];
+};
